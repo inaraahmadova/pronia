458f8dfa9699c8a7e340f95e99e0be3401903fe8
inaraahmadova <tu7r5hs82@code.edu.az>
2024-05-08 20:44:11 +0400

    first commit

---
+++ Pronia/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pronia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Shop()
+        {
+            return View();
+        }
+    }
+}
